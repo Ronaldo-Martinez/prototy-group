ad6ceb3990178ada5e62b7624df4a4c386ef6b2b
RonalMtz <ronaldo1103mtz@gmail.com>
2021-04-14 01:42:13 -0600

    Nosotros. html

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+    sr.reveal('.navbar', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.page-section', {
+        duration: 2000,
+        origin: 'up',
+        distance: '-100px'
+    });
+    sr.reveal('.portafolio', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-100px'
+    });
